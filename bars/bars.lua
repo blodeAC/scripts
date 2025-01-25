26d7591969281e5837e9ed2355ccf375e96a57c7
blode <acbreyer@gmail.com>
2025-01-25 13:19:06 -0600

    Update bars.lua

--- bars/bars.lua
+++ bars/bars.lua
@@ -1936,7 +1936,36 @@ bars({
       end
     end,
     render = renderBuffs
-  }, --]]
+  },
+  { name = "HealkitCooldown", --important, must be correctly capitalized for enum to work
+    settings = {
+      enabled = false,
+      icon_hex = 0x060032E5,
+      fontScale_flt = 2,
+    },
+    init = function(bar)
+      ---@param cooldownChanged SharedCooldownsChangedEventArgs
+      game.Character.OnSharedCooldownsChanged.Add(function(cooldownChanged)
+        local maybeKit=game.World.Get(cooldownChanged.Cooldown.ObjectId)
+
+        if maybeKit and maybeKit.ObjectClass==ObjectClass.HealingKit then
+          bar.cooldown = cooldownChanged.Cooldown.ExpiresAt
+        end
+      end)
+    end,
+    render = function(bar)
+      if bar.cooldown then
+        local rem = (bar.cooldown - DateTime.UtcNow).TotalSeconds
+        if rem > 0 then
+          bar.settings.label_str = string.format("%.1f", rem)
+        else
+          bar.cooldown = nil
+          bar.settings.label_str = nil
+        end
+      end
+      DrawIcon(bar,bar.settings.icon_hex)
+    end
+  },
 })
 
 return bars
